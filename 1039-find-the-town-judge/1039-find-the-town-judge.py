ad09cc4a9c8d5c756cd337f49f90a14c005ef83e
Cole Hanlon <cole.hanlon28@gmail.com>
2023-09-21 22:36:43 -0500

    Time: 658 ms (85.87%), Space: 21.9 MB (84.02%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -1,14 +1,14 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         trust_me = defaultdict(int)
-        i_trust = defaultdict(int)
+        i_trust = set()
 
         for relationship in trust:
-            i_trust[relationship[0]] += 1
+            i_trust.add(relationship[0])
             trust_me[relationship[1]] += 1
 
         for person in range(1, n+1):
-            if trust_me[person] == n - 1 and i_trust[person] == 0:
+            if trust_me[person] == n - 1 and person not in i_trust:
                 return person
             
         return -1
