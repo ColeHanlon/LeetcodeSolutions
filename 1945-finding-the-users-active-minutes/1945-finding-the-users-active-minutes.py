4fabb5da4792519a46f997db0028a0b5a8d6c2fc
Cole Hanlon <cole.hanlon28@gmail.com>
2023-09-25 23:16:40 -0500

    Time: 831 ms (43.52%), Space: 27.3 MB (31.63%) - LeetHub

--- 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.py
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.py
@@ -1,13 +1,15 @@
 class Solution:
     def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
         user_minutes = defaultdict(set)
-
-        for i in range(len(logs)):
-            user_minutes[logs[i][0]].add(logs[i][1])
-
         num_users = [0] * k
 
-        for key, item in user_minutes.items():
-            num_users[len(item) - 1] += 1
+        for i in range(len(logs)):
+            existing = len(user_minutes[logs[i][0]])
+            user_minutes[logs[i][0]].add(logs[i][1])
+
+            if existing > 0:
+                num_users[existing - 1] -= 1
+            
+            num_users[len(user_minutes[logs[i][0]]) - 1] += 1
 
         return num_users
