7a569469c2280c89a4d67902dbbfa2ada7318d3f
Cole Hanlon <cole.hanlon28@gmail.com>
2023-10-25 17:31:40 -0500

    Time: 44 ms (19.31%), Space: 16.1 MB (97.3%) - LeetHub

---
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        # 1 - 0
+        # 2 - 0 1
+        # 3 - 01 10 - Inverted
+        # 4 - 0110 1001 - Inverted
+        # 5 - 01101001 10010110 - Inverted
+        if n == 1:
+            return 0
+        length = 2 ** (n - 2)
+        if k <= length:
+            return self.kthGrammar(n - 1, k)
+        else:
+            return 1 - self.kthGrammar(n - 1, k - length)
