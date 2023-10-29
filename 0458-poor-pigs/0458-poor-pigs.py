3911adbb60c1d275726f6b0dbde016282806f37d
Cole Hanlon <cole.hanlon28@gmail.com>
2023-10-28 22:16:11 -0500

    Time: 36 ms (68.8%), Space: 16.3 MB (13.6%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        time = minutesToTest / minutesToDie + 1
+
+        pigs = 0
+        while time ** pigs < buckets:
+            pigs += 1
+        
+        return pigs
